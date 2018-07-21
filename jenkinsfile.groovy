aa6482a86e989142abffa7aa96b7bbd1fac78d8f
Zajs <aga.zajs@gmail.com>
2018-07-21 18:45:04 +0300

    check for junit and warnings

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -127,9 +127,9 @@ node("JenkinsOnDemand") {
             sh "sbt -DappVersion=${curVersion} it:testOnly"
         } finally {
             def  junitResult=junit testResults: '**/target/test-reports/io.hydrosphere*.xml', allowEmptyResults: true
-            echo junitResult
+            echo "${junitResult}"
             def wa=warnings
-            echo wa
+            echo "${wa}"
         }
     }
     if (isReleaseJob()) {
